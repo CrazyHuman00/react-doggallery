37a7c4c12edea1b63625e82b4bde6b3ec10e576d
CrazyHuman00 <asakurashi00@icloud.com>
2024-04-11 23:53:40 +0900

    完成

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # react-doggallery
+
+## Reactのチュートリアル
+DogAPIを使って犬の画像を表示するwebサイト
+
+↓できたやつ
+<br>
+https://6617f7a3a33ae11d4b9fc5f5--playful-strudel-ac6cf2.netlify.app/
+
+## 参考文献
+[Reactチュートリアル1：犬画像ギャラリーを作ろう](https://zenn.dev/likr/articles/6be53ca64f29aa035f07#%E6%9C%AC%E8%B3%87%E6%96%99%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6)
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchImages } from "./api"
 
 // Headerのコンポーネント
@@ -51,20 +51,60 @@ function Gallery(props) {
     );
 }
 
-function Main() {
-    const urls = null;
-    useEffect( () => {
-        fetchImages("shiba").then((urls) => {
-            console.log(urls);
-        });
-        },
-        []
+// Formコンポーネント
+function Form(props) {
+    function handleSubmit(event) {
+        event.preventDefault();
+        const { breed } = event.target.elements;
+        props.onFormSubmit(breed.value);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="field has-addons">
+                    <div className="control is-expanded">
+                        <div className="select is-fullwidth">
+                            <select name="breed" defaultValue="shiba">
+                                <option value="shiba">Shiba</option>
+                                <option value="akita">Akita</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="control">
+                        <button type="submit" className="button is-dark">
+                            Reload
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
     );
+}
+
+function Main() {
+    const [urls, setUrls] = useState(null);
+    useEffect(() => {
+        fetchImages("shiba").then((urls) => {
+            setUrls(urls);
+        });
+    }, []);
+
+    function reloadImages(breed) {
+        fetchImages(breed).then((urls) => {
+            setUrls(urls);
+        });
+    }
+
     return (
         <main>
             <section className="section">
                 <div className="container">
-                    <Gallery urls={urls} />
+                    <Form onFormSubmit={reloadImages} />
+                </div>
+            </section>
+            <section className="section">
+                <div className="container">
+                    <Gallery urls={urls}/>
                 </div>
             </section>
         </main>
